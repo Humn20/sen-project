5cd8426c3c5388ec458662d413119aa80c292ba6
emmafguarnieri <95932570+emmafguarnieri@users.noreply.github.com>
2024-02-21 16:39:22 -0500

    added clickable buttons

--- amplifyapp/src/Page.js
+++ amplifyapp/src/Page.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -5,6 +6,8 @@ import Button from "react-bootstrap/Button";
 import DNSComponent from "./DNSComponent";
 import Navbar from "react-bootstrap/Navbar";
 import { useContext } from "react";
+
+//let global = true;
 
 function NavHeader() {
   return (
@@ -21,6 +24,8 @@ function NavHeader() {
 }
 
 function Page() {
+  let [global, setGlobal] = React.useState(true);
+
   return (
     <>
       <Container fluid>
@@ -28,10 +33,10 @@ function Page() {
         <Row>
           <Col md={4}>
             <div className="d-grid gap-2">
-              <Button variant="primary" size="lg">
+              <Button variant={global ? "secondary" : "primary"} size="lg" onClick={() => {setGlobal(false);}}>
                 Local
               </Button>
-              <Button variant="secondary" size="lg">
+              <Button variant={global ? "primary" : "secondary"} size="lg" onClick={() => {setGlobal(true);}}>
                 Global
               </Button>
               <div>
