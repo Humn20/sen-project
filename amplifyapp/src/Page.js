a007046ca72e6ffe7c1fd95a92a29dbed6b45b0e
nicoguerra18 <139820627+nicoguerra18@users.noreply.github.com>
2024-03-01 15:16:34 -0500

    resolving eslint

--- amplifyapp/src/Page.js
+++ amplifyapp/src/Page.js
@@ -34,7 +34,7 @@ function NavHeader() {
             <path d="M1.333 6.334v3C1.333 10.805 4.318 12 8 12s6.667-1.194 6.667-2.667V6.334a6.5 6.5 0 0 1-1.458.79C11.81 7.684 9.967 8 8 8s-3.809-.317-5.208-.876a6.5 6.5 0 0 1-1.458-.79z" />
             <path d="M14.667 11.668a6.5 6.5 0 0 1-1.458.789c-1.4.56-3.242.876-5.21.876-1.966 0-3.809-.316-5.208-.876a6.5 6.5 0 0 1-1.458-.79v1.666C1.333 14.806 4.318 16 8 16s6.667-1.194 6.667-2.667z" />
           </svg>
-          &nbsp; <b>Periodic Domain Name Server Resolver Recommender</b>
+          &nbsp;<b>Periodic Domain Name Server Resolver Recommender</b>
         </div>
       </Navbar.Brand>
     </Navbar>
