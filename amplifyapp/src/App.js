9b43353ab5c5d99be0dd0f2b0f4e21b00c6b8376
emmafguarnieri <95932570+emmafguarnieri@users.noreply.github.com>
2024-04-11 20:28:59 -0400

    removed the header

--- amplifyapp/src/App.js
+++ amplifyapp/src/App.js
@@ -3,7 +3,6 @@ import Page from "./Components/Page";
 function App() {
   return (
     <div>
-      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
       <Page />
     </div>
   );
