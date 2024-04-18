e7a4e69adc01e36992329fbb0b25bd93c54d369d
emmafguarnieri <95932570+emmafguarnieri@users.noreply.github.com>
2024-04-18 14:00:58 -0400

    backend stuff

---
+++ amplifyapp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,11 @@
+export type AmplifyDependentResourcesAttributes = {
+  "function": {
+    "apiGetRequest": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
